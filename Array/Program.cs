02c1d20d0c6133ef3cdc23a806ade430cdcc1cb4
antonnn1 <antonnn@vgy.se>
2016-10-21 15:04:11 +0200

    ovn3

--- Array/Program.cs
+++ Array/Program.cs
@@ -25,6 +25,7 @@ namespace Array
             }
             */
 
+            /*
             //Övn 2
             int[] tal = new int[10];
             for (int i = 0; i < 10; i++)
@@ -37,6 +38,27 @@ namespace Array
             {
                 Console.WriteLine(tal[i]);
             }
+            */
+
+            //Övn 3
+            int mellan = 0; 
+            int[] numbArray = new int[] { 12, 5, 6, 1, 2, 3, -2, -7, 0 };
+            for (int i = 0; i < numbArray.Length; i++)
+            {
+                for (int x = i + 1; x < numbArray.Length; x++)
+                {
+                    if (numbArray[i] > numbArray[x])
+                    {
+                        mellan = numbArray[x];
+                        numbArray[x] = numbArray[i];
+                        numbArray[i] = mellan;
+                    }
+                }
+                Console.WriteLine(numbArray[i]);
+            
+            }
+            
+                
 
 
 
